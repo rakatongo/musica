546d6d7d40e701732a35659af703f71b329efd0b
Rakunho <rakatongo@hotmail.com>
2012-12-16 06:07:29 -0300

    comitting

--- app/assets/javascripts/bg.js
+++ app/assets/javascripts/bg.js
@@ -1,39 +1,20 @@
-var links = [
-			"http://i.imgur.com/kpfI5.jpg ",
+var links = [			
 			"http://i.imgur.com/IJDui.jpg",
 			"http://i.imgur.com/aA3m5.jpg",
 			"http://i.imgur.com/3VT9U.jpg",			
-			"http://i.imgur.com/oJfGa.jpg", 
+			"http://i.imgur.com/oJfGa.jpg",
+			"http://i.imgur.com/kpfI5.jpg", 
 			"http://i.imgur.com/mJNjo.jpg",			
 			"http://i.imgur.com/U8TkE.jpg",
 			"http://i.imgur.com/xC16W.jpg"
 		]
 var current = 0	
-function thebackground() {
-	
-	setInterval('change()',25000);
-	
+function thebackground() {	
+	setInterval('change()',6000);	
 }
 
-function change() {		
-	var img = new Image()
-	img.src = links[current]	
-	img.onload = function() {
-		$('div.background img').fadeOut('fast',
-			function() {
-				$(this).remove()
-				var bg = $('div.background')
-				bg.hide()
-				.html(img)
-				var imagen = $('div.background img')
-				imagen.css({opacity: 0.0})		
-				imagen.appendTo().addClass('show')
-				bg.show()
-				imagen.animate({opacity: 1.0}, 200);
-			}
-
-		)		
-	}	
+function change() {			
+	$('body').css('background-image','url('+ links[current]+')')
 	current ++	
 	if (current >= links.length ) {
 		current = 0
@@ -41,7 +22,5 @@ function change() {
 	
 };
 $(document).ready(function() {
-	thebackground();
-	$('div.background').fadeIn(1000); // works for all the browsers other than IE
-	$('div.background img').fadeIn(1000); // IE tweak
+	thebackground();	
 });
